0128251a83916871c68d7f913e428a5ccafafa5d
Bharat Gupta <43195065+bharatgupta77@users.noreply.github.com>
2022-02-13 04:37:00 +0530

    Time: 88 ms, Memory: 13.9 MB - LeetHub

--- find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -3,13 +3,20 @@ class Solution:
         
         count = 0
         for i in nums:
-            no_digits = 0
-            while(i>0):
-                no_digits +=1
-                i = i//10
-            if no_digits % 2 == 0:
-                count+=1
+            if(len(str(i)) % 2 ==0) : count+=1
         return count
+    
+        #Logical approach
+        
+        # count = 0
+        # for i in nums:
+        #     no_digits = 0
+        #     while(i>0):
+        #         no_digits +=1
+        #         i = i//10
+        #     if no_digits % 2 == 0:
+        #         count+=1
+        # return count
 
         
         
