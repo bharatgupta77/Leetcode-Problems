6c12ecd55b8741bc3df3a0a9a055791c4bcbbd88
Bharat Gupta <43195065+bharatgupta77@users.noreply.github.com>
2022-02-13 04:33:44 +0530

    Time: 104 ms, Memory: 13.9 MB - LeetHub

--- find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -3,12 +3,10 @@ class Solution:
         
         count = 0
         for i in nums:
-            print(i)
             no_digits = 0
             while(i>0):
                 no_digits +=1
                 i = i//10
-                print("inside",i)
             if no_digits % 2 == 0:
                 count+=1
         return count
