ff78a2c62bc52b09f78c6f3cca91347b5a7e2610
Bharat Gupta <43195065+bharatgupta77@users.noreply.github.com>
2022-02-27 13:57:36 +0530

    Time: 240 ms, Memory: 15.8 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.py
+++ valid-mountain-array/valid-mountain-array.py
@@ -2,14 +2,19 @@ class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
         if len(arr) < 3:
             return False
+        
+        #my solution
+        
         x=arr[0]
         mid_index = 0
         
-        #to check whether there is only decreasing slope
-        k = arr[:]
-        k.sort(reverse = True)
-        if (k == arr):
-            return False
+        # #to check whether there is only decreasing slope
+        # k = arr[:]
+        # k.sort(reverse = True)
+        # if (k == arr):
+        #     return False
+        
+        
         
         for i in range(1,len(arr)):
             if x<arr[i]:
@@ -20,12 +25,37 @@ class Solution:
             else:
                 mid_index = i-1
                 break
+        
+        if(mid_index == 0 or mid_index == len(arr)-1):
+            return False;
+        
+        
                 
         #after getting highest point will check the decrasing slope
         l = arr[mid_index:]
         l.sort(reverse = True)
         if (l == arr[mid_index:] and len(set(l)) == len(l)):
             return True
+
+
+          #simple approach (time complexity will be more)
+            
+#         n = len(arr);
+#         i = 0;
+
+#         #walk up
+#         while (i+1 < n and arr[i] < arr[i+1]):
+#             i+=1;
+
+#         #peak can't be first or last
+#         if(i == 0 or i == n-1):
+#             return False;
+
+#         #walk down
+#         while(i+1 < n and arr[i] > arr[i+1]):
+#             i+=1;
+
+#         return i == n-1;
             
             
                 
