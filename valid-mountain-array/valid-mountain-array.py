2890d742e3554545fde0abdba26d5ff170ea167f
Bharat Gupta <43195065+bharatgupta77@users.noreply.github.com>
2022-02-27 13:45:22 +0530

    Time: 296 ms, Memory: 15.7 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.py
+++ valid-mountain-array/valid-mountain-array.py
@@ -1,9 +1,11 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
         x=arr[0]
         mid_index = 0
         
-        
+        #to check whether there is only decreasing slope
         k = arr[:]
         k.sort(reverse = True)
         if (k == arr):
@@ -18,6 +20,8 @@ class Solution:
             else:
                 mid_index = i-1
                 break
+                
+        #after getting highest point will check the decrasing slope
         l = arr[mid_index:]
         l.sort(reverse = True)
         if (l == arr[mid_index:] and len(set(l)) == len(l)):
