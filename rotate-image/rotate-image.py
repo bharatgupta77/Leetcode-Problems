016e8efa0caa18da7087665bdcc720831184d498
Bharat Gupta <43195065+bharatgupta77@users.noreply.github.com>
2023-05-25 01:16:56 +0530

    Time: 20 ms, Memory: 13.5 MB - LeetHub

--- rotate-image/rotate-image.py
+++ rotate-image/rotate-image.py
@@ -5,10 +5,12 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
         
+        #transpose the matrix first
         for i in range(len(matrix)):
             for j in range(i+1,len(matrix)):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
+        #reverse the rows
         for k in range(len(matrix)):
             matrix[k].reverse()
                            
