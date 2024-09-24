16e7123077a939cb447f662231035cf711ff7986
Bharat Gupta <43195065+bharatgupta77@users.noreply.github.com>
2024-09-23 21:35:28 -0500

    Time: 1951 ms, Memory: 74.1 MB - LeetHub

---
+++ count-primes/count-primes.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        cnt=0
+        if n == 2:
+            return 0
+        l= [True for i in range(n+1)]
+        a = 2
+        while (a*a) <= n:
+            if (l[a] == True):
+                for i in range(a * a, n+1, a):
+                    l[i] = False
+            a += 1
+        
+        for j in range(2,n):
+            if l[j]:
+                cnt+=1
+        return cnt
+
+        
+        
